26600e53d9a0ef33fb6a5f2c1c79469893f63408
NamSangKyu <nam2626@naver.com>
2022-02-28 17:42:37 +0900

    블로그 검색 앱

--- android/NaverSearch/app/src/main/java/com/nsk/naversearch/MainActivity.java
+++ android/NaverSearch/app/src/main/java/com/nsk/naversearch/MainActivity.java
@@ -4,10 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,13 +39,72 @@ public class MainActivity extends AppCompatActivity {
         listResult = findViewById(R.id.lst_result);
         adapter = new CustomAdapter();
         listResult.setAdapter(adapter);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SearchTask task = new SearchTask();
+                task.execute(edtSearch.getText().toString());
+            }
+        });
     }
 
     public class SearchTask extends AsyncTask<String, String, ArrayList<ItemVO>>{
 
         @Override
         protected ArrayList<ItemVO> doInBackground(String... strings) {
-            return null;
+            ArrayList<ItemVO> list = new ArrayList<ItemVO>();
+            String result = null;
+            String clientId = "9IEElpSYKwxvuax9FsIh";
+            String clientSecret = "bBrxBHQPu0";
+            String apiUrl = "https://openapi.naver.com/v1/search/blog.json";
+            HttpURLConnection con = null;
+            BufferedReader br = null;
+            String text = strings[0];
+            try {
+                text = URLEncoder.encode(text, "UTF-8");
+                URL url = new URL(apiUrl+"?query="+text);
+                con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("X-Naver-Client-Id", clientId);
+                con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+                //con.setDoOutput(true);//post일때만 적용
+
+                int responseCode = con.getResponseCode();
+                if(responseCode == HttpURLConnection.HTTP_OK) {
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                }else {
+                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                }
+
+                result = new String();
+
+                while (true) {
+                    String str = br.readLine();
+                    if(str == null) break;
+                    result += str;
+                }
+                Log.d("SEARCH", "doInBackground: "+result);
+                JSONObject json = new JSONObject(result);
+                JSONArray arr = json.getJSONArray("items");
+                for(int i=0;i<arr.length();i++) {
+                    JSONObject obj = (JSONObject) arr.get(i);
+                    String title =obj.getString("title");
+                    String description =obj.getString("description");
+                    String link =obj.getString("link");
+                    list.add(new ItemVO(title,description,link));
+                }
+                Log.d("SEARCH", "doInBackground: "+list.toString());
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return list;
         }
 
         @Override
