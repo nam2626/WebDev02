5682d7f47c4563116f6cd98319239ad050892b45
NamSangKyu <nam2626@naver.com>
2022-03-03 16:16:43 +0900

    엑셀 파일 읽기 예제

---
+++ jdk/ExcelLoad/src/ExcelMain.java
@@ -0,0 +1,38 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.util.ZipSecureFile;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelMain {
+
+	public static void main(String[] args) {
+		ZipSecureFile.setMinInflateRatio(0);
+		try {
+			FileInputStream fis = new FileInputStream("position.xlsx");
+			XSSFWorkbook workbook = new XSSFWorkbook(fis);
+			//엑셀 시트 선택
+			XSSFSheet sheet = workbook.getSheetAt(0);//엑셀 시트 순번으로 읽어옴
+			
+			System.out.println("전체 행 개수 : " + sheet.getPhysicalNumberOfRows());
+			System.out.println("해당 라인의 컬럼수 : "+sheet.getRow(0).getPhysicalNumberOfCells());
+			System.out.println("4행 5열에 있는 셀값 : " + sheet.getRow(3).getCell(4));
+			
+			for(int i=0;i<sheet.getPhysicalNumberOfRows();i++) {
+				String row = "";
+				for(int j=0;j<sheet.getRow(0).getPhysicalNumberOfCells();j++) {
+					row += sheet.getRow(i).getCell(j) +"\t";
+				}
+				System.out.println(row);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
